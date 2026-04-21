fdfc583e35eb7a005ac155f6c104aae7b1b764a2
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-21 14:59:25 -0400

    comment structure for code

--- src/main/java/com/pluralsight/Employees.java
+++ src/main/java/com/pluralsight/Employees.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
 public class Employees {
+
+    // Create employee Object
+    //Create employee Constructor
+    // Getters
+    //Setters
+    // Ending method for gross pay
 }
