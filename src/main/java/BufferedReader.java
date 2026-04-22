08c59e5230539ea96d082f7e30d096ca268b1907
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-22 10:29:46 -0400

    try/catch

--- src/main/java/BufferedReader.java
+++ src/main/java/BufferedReader.java
@@ -1,16 +1,24 @@
+import java.io.IOException;
+
 public class BufferedReader {
     public static void main(String[] args) {
         //File reafder
         String fileName = "employees.csv";
 
-        // Open file
-        //Read file with buffer
-        //Variable to hold lines
-        // While loop to keep reading while lnes are true
-        // Store each line part into variables
-        // New employee object
-        // Print New Onject
-        // try/catch
+        try {
+
+            // Open file
+            //Read file with buffer
+            //Variable to hold lines
+            // While loop to keep reading while lnes are true
+            // Store each line part into variables
+            // New employee object
+            // Print New Onject
+        } catch(IOException e){
+            System.out.println("Error reading file.");
+        }
+            // try/catch
+
 
 
     }
